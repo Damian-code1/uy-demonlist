e47580eb62ae8099804a7f571aaf6f4515545134
Damian-code1 <cssdamian2@gmail.com>
2026-06-18 22:13:37 -0300

    fix: ruta db en discord callback

--- app/api/auth/callback/discord/route.js
+++ app/api/auth/callback/discord/route.js
@@ -1,4 +1,4 @@
-import { query } from '../../../../lib/db.js';
+import { query } from '../../../../../lib/db.js';
 
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
